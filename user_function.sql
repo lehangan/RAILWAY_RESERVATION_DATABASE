ae8cc03850fc6bdc95a3798be00ace9a98e6142b
lehangan <lehangan30@gmail.com>
2024-01-01 22:13:39 +0700

    feat: add function get information

--- user_function.sql
+++ user_function.sql
@@ -1,4 +1,47 @@
----1. Function to get all seat with schedule id
+--1. Function get information about station_name and get station_id
+CREATE OR REPLACE FUNCTION check_station(station_name1 VARCHAR)
+RETURNS varchar -- Assuming station_id is of INTEGER type
+AS 
+$$
+DECLARE
+    station_id1 varchar;
+BEGIN
+    SELECT station_id INTO station_id1
+    FROM station
+    WHERE station_name = station_name1;
+
+    -- If the station is not found, you might want to handle that case
+    IF station_id1 IS NULL THEN
+        RETURN NULL; -- or handle it in another way
+    END IF;
+
+    RETURN station_id1;
+END
+$$
+LANGUAGE plpgsql;
+
+
+--2. Funciton get information about schedule 
+CREATE OR REPLACE FUNCTION show_schedule(station_from VARCHAR, station_to VARCHAR, depart_time DATE)
+RETURNS TABLE (from1 VARCHAR, to1 VARCHAR, depart TIMESTAMP, arrival TIMESTAMP)
+AS  
+$$ 
+DECLARE 
+    from_name VARCHAR;
+    to_name VARCHAR;
+BEGIN 
+    SELECT check_station(station_from) INTO from_name;
+    SELECT check_station(station_to) INTO to_name;
+
+    RETURN QUERY
+    SELECT  station_from, station_to , departure_time, arrival_time  
+    FROM train_schedule 
+    WHERE station_from_id = from_name AND station_to_id = to_name AND date(arrival_time) = depart_time;
+END
+$$
+LANGUAGE plpgsql
+
+---3. Function to get all seat with schedule id
 CREATE OR REPLACE FUNCTION get_seat_all(schedule_id integer)
 RETURNS TABLE(
 	seat_id integer,
@@ -17,7 +60,7 @@ $$
 LANGUAGE plpgsql;
 
 
---2. Function to get all seat are booked with schedule_id
+--4. Function to get all seat are booked with schedule_id
 CREATE OR REPLACE FUNCTION check_overlap_integer(range1_start INT, range1_end INT, range2_start INT, range2_end INT)
 RETURNS BOOLEAN AS $$
 BEGIN
@@ -60,7 +103,7 @@ END;
 $$
 LANGUAGE plpgsql;
 
---3. Function to get all seat are empty with schedule_id
+--5. Function to get all seat are empty with schedule_id
 CREATE OR REPLACE FUNCTION get_seat_empty(schedule_id integer)
 RETURNS TABLE(
 	seat_id integer,
