ffffd34a2ac71203409278ee618966453f3edce7
lehangan <lehangan30@gmail.com>
2024-01-02 00:13:50 +0700

    feat: file update

--- alter_add_contraint_table.sql
+++ alter_add_contraint_table.sql
@@ -8,3 +8,9 @@ alter table ticket
 add constraint fk_ticket_passenger_id foreign key (passenger_id) references passenger(passenger_id) on delete set null,
 add constraint fk_ticket_seat_id foreign key(seat_id) references seat(seat_id) on delete cascade,
 add constraint fk_ticket_train_schedule_id foreign key(schedule_id) references train_schedule(schedule_id) on delete cascade;
+
+ALTER TABLE passenger 
+ADD CONSTRAINT constraint_phone UNIQUE (phone);
+
+ALTER TABLE passenger 
+ADD CONSTRAINT constraint_email UNIQUE (email);
