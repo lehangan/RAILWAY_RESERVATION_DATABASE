780fc054b2f51a9c3d6e702d6b27aaa67cc7380d
Paper5923 <111866827+Paper5923@users.noreply.github.com>
2024-01-02 13:49:46 +0700

    Add files via upload

---
+++ SignUp_In.sql
@@ -0,0 +1,116 @@
+
+-- Add constraint to email and phone 
+ALTER TABLE passenger 
+ADD CONSTRAINT constraint_phone UNIQUE (phone);
+
+ALTER TABLE passenger 
+ADD CONSTRAINT constraint_email UNIQUE (email);
+------------
+CREATE OR REPLACE FUNCTION Sign_Up(phone1 varchar, email1 varchar, password1 varchar, name1 varchar, dob1 date)
+RETURNS void AS
+$$
+BEGIN
+    INSERT INTO passenger( phone, email, password, name, dob) 
+    VALUES(phone1, email1, password1, name1, dob1);
+END;
+$$
+LANGUAGE plpgsql;
+
+--- Sign In , show info if ok
+CREATE OR REPLACE FUNCTION Sign_In(emailOrphone varchar, password1 varchar)
+RETURNS TABLE (
+    passenger_id integer,
+    name varchar,
+    password varchar,
+	email varchar,
+	phone varchar
+    -- Add other columns as needed
+	)
+AS
+$$
+    
+    SELECT passenger_id, name, password, email, phone 
+    FROM passenger
+    WHERE (phone = emailOrPhone  OR email = emailOrphone) AND password = password1;
+
+$$
+LANGUAGE sql;
+
+----Check admin, give notification 
+
+CREATE OR REPLACE FUNCTION CheckAdmin(name1 varchar, password1 varchar)
+RETURNS VOID
+AS
+$$
+DECLARE
+    Admin_exists boolean;
+    CorrectPassword boolean;
+BEGIN
+    SELECT EXISTS (
+        SELECT 1
+        FROM admin_railway
+        WHERE account = name1
+    ) INTO Admin_exists;
+
+    IF Admin_exists THEN
+        SELECT EXISTS (
+            SELECT 1
+            FROM admin_railway
+            WHERE account = name1 AND password = password1
+        ) INTO CorrectPassword;
+
+        IF CorrectPassword THEN
+            -- Correct name and correct password
+            RAISE NOTICE 'Notification: Admin information found for name: %', name1;
+        ELSE
+            -- Correct name, but incorrect password
+            RAISE NOTICE 'Notification: Incorrect Password for name: %', name1;
+        END IF;
+    ELSE
+        -- Incorrect name
+        RAISE NOTICE 'Notification: Admin information not found for name: %', name1;
+    END IF;
+END;
+$$
+LANGUAGE plpgsql;
+
+-- -------return a number 
+-- CREATE OR REPLACE FUNCTION CheckAdmin(name1 varchar, password1 varchar)
+-- RETURNS integer
+-- AS
+-- $$
+-- DECLARE
+--     Admin_exists boolean;
+--     CorrectPassword boolean;
+-- BEGIN
+--     SELECT EXISTS (
+--         SELECT 1
+--         FROM admin_railway
+--         WHERE account = name1
+--     ) INTO Admin_exists;
+
+--     IF Admin_exists THEN
+--         SELECT EXISTS (
+--             SELECT 1
+--             FROM admin_railway
+--             WHERE account = name1 AND password = password1
+--         ) INTO CorrectPassword;
+
+--         IF CorrectPassword THEN
+--             -- Correct name and correct password
+--             RETURN 1;
+--         ELSE
+--             -- Correct name, but incorrect password
+--             RETURN 2;
+--         END IF;
+--     ELSE
+--         -- Incorrect name
+--         RETURN 3;
+--     END IF;
+-- END;
+-- $$
+-- LANGUAGE plpgsql;
+
+
+
+
